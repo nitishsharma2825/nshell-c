469e421f916d2be1a1081b70b872a17f6f3bebb8
nitishsharma2825 <sharnit2825@gmail.com>
2024-10-02 00:55:07 +0530

    added support for exit and clear commands

--- commands.c
+++ commands.c
@@ -4,5 +4,18 @@
 
 void executeCommands(char *command)
 {
-    //Add your code here
+    if (strncmp(command, "exit", 4) == 0)
+    {
+        fprintf(stdout, "Closing edsh\033[5m...\033[0m\n");
+        sleep(1);
+        exit(0);
+    }
+    else if (strncmp(command, "clear", 5) == 0)
+    {
+        printf("\033[2J\033[1;1H");
+    }
+    else
+    {
+        printf("\033[1;31mInvalid Command\n\033[0m");
+    }  
 }
